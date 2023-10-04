f2e1fac2999d0585be18c01a1a31f42d01bcce8e
Akshay164-lab <143415045+Akshay164-lab@users.noreply.github.com>
2023-10-04 08:31:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ if SELF_DELETE == "True":
 
 # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://t.me/Flimshowcase/8"
+DOWNLOAD_TEXT_URL = "https://t.me/Film_ShowCase/6"
 
 # Custom Caption Under Button #
 CAPTION_BUTTON = "Get Updates"
